907cc6b12b8388de640f31f4dc12461414f14762
diego2606 <diego.velasquez.ore@gmail.com>
2022-10-21 19:21:38 -0500

    Añadimos el script del .env

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import Template from '@templates/Template.js';
 import '@styles/main.css';
 import '@styles/vars.styl';
 
-console.log('Hola');
-console.log('hey')
-console.log('sadsa');
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
